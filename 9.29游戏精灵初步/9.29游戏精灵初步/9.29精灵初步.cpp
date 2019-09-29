15495702c20d0f5564cce683fde8277e4a113dbb
Colt-M1873 <47551017+Colt-M1873@users.noreply.github.com>
2019-09-30 00:10:20 +0800

    .。

--- 9.29游戏精灵初步/9.29游戏精灵初步/9.29精灵初步.cpp
+++ 9.29游戏精灵初步/9.29游戏精灵初步/9.29精灵初步.cpp
@@ -1,7 +1,39 @@
 #include "acllib.h"
 const int winWidth = 640, winHeight = 480;
+ACL_Image img1;
+const char *tom = "1.jpg";
+void timerEvent(int id);
+int id = 0 ,timerInterval = 50;
+int x = 10, y = 10, dist = 5;
+void paint();
+
 int Setup(void)
 {
 	initWindow("window1", DEFAULT, DEFAULT, winWidth, winHeight);
+	loadImage(tom, &img1);
+	beginPaint();
+	putImageScale(&img1, 0,0,100,100);
+	registerTimerEvent(timerEvent);
+	startTimer(id, timerInterval);
+	//paint();//为什么加这一句会出错？
+	endPaint();
 	return 0;
+
 }
+void timerEvent(int id) {
+	if (id == 0) {
+		x += 5;
+		y += 5;
+		paint();
+	}
+
+}
+void paint()
+{
+	beginPaint();
+	putImageScale(&img1, x, y, 100, 100);
+	endPaint();
+	return ;
+
+}
+
