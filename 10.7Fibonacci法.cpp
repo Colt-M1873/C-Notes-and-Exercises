c781a1571f894459bb88c387be11f9b2628168e8
Colt-M1873 <47551017+Colt-M1873@users.noreply.github.com>
2019-10-07 22:24:21 +0800

    Create 10.7Fibonacci法.cpp

---
+++ 10.7Fibonacci法.cpp
@@ -0,0 +1,43 @@
+/*170603108 自动化17-1 杨佳男*/
+#include<stdio.h>
+#include<stdlib.h>
+double fun(double x);
+double Fibonacci(double(*fun)(double), int n, double left, double right);
+
+int main()
+{
+	double result;
+	double(*f)(double);
+	f = fun;
+	result = Fibonacci(f, 6, -1, 20);
+	printf("%f", result);
+	system("pause");
+}
+
+double fun(double x)
+{
+	return x * x - 6 * x + 9;
+}
+
+double Fibonacci(double(*fun)(double),int n, double left, double right)
+{
+	//*fun为指向待求函数的函数指针,n为计算轮次，left right 为区间端点
+	double fib[50];//斐波那契数列
+	double x1, x2;
+	fib[0] = 1;
+	fib[1] = 1;
+	for (int i = 2; i < 50; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+	for (int j = 0; j < n; j++)
+	{
+		x1 = left + fib[n - 2] / fib[n] * (right - left);
+		x2 = left + fib[n - 1] / fib[n] * (right - left);
+		if ((*fun)(x1) < (*fun)(x2))
+			right = x2;
+		else
+			left = x1;
+	}
+	return 0.5*(left + right);
+}
