83482c9905d538bce78b40025490e50580c18e69
Colt-M1873 <47551017+Colt-M1873@users.noreply.github.com>
2019-10-07 23:08:47 +0800

    Update 10.7 0.618法.cpp

--- 10.7 0.618法.cpp
+++ 10.7 0.618法.cpp
@@ -1,8 +1,9 @@
 /*170603108 自动化17-1 杨佳男*/
 #include<stdio.h>
 #include<stdlib.h>
-double fun(double x);
+double fun(double x);//此为待求取的函数
 double Golden(double(*fun)(double), double left, double right, double precision);
+//*fun为指向待求函数的函数指针,n为计算轮次，left right 为区间端点,precision为精度值
 
 int main()
 {
@@ -10,7 +11,7 @@ int main()
 	double(*f)(double);
 	f = fun;
 	result = Golden(f, -1, 10, 0.003);
-	printf("%f", result);
+	printf("函数最小值为 %f \n对应的x值为 %f", fun(result), result);
 	system("pause");
 }
 
@@ -25,7 +26,6 @@ double Golden(double(*fun)(double), double left, double right, double precision)
 	double x1, x2;
 	while (right - left > precision)
 	{
-
 		x1 = left + 0.382 * (right - left);
 		x2 = left + 0.618 * (right - left);
 		if ((*fun)(x1) < (*fun)(x2))
