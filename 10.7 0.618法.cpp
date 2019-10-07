0f1c1c37661d83f4a1c4a23a4aeebe64dc3c3997
Colt-M1873 <47551017+Colt-M1873@users.noreply.github.com>
2019-10-07 22:33:07 +0800

    Create 10.7 0.618法.cpp

---
+++ 10.7 0.618法.cpp
@@ -0,0 +1,37 @@
+/*170603108 自动化17-1 杨佳男*/
+#include<stdio.h>
+#include<stdlib.h>
+double fun(double x);
+double Golden(double(*fun)(double), double left, double right, double precision);
+
+int main()
+{
+	double result;
+	double(*f)(double);
+	f = fun;
+	result = Golden(f, -1, 10, 0.003);
+	printf("%f", result);
+	system("pause");
+}
+
+double fun(double x)
+{
+	return x * x - 6 * x + 9;
+}
+
+double Golden(double(*fun)(double), double left, double right, double precision)
+{
+	//*fun为指向待求函数的函数指针,n为计算轮次，left right 为区间端点,precision为精度值
+	double x1, x2;
+	while (right - left > precision)
+	{
+
+		x1 = left + 0.382 * (right - left);
+		x2 = left + 0.618 * (right - left);
+		if ((*fun)(x1) < (*fun)(x2))
+			right = x2;
+		else
+			left = x1;
+	}
+	return 0.5*(left + right);
+}
